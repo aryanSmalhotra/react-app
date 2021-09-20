4cccbcd5297d5f8622177005840f90c074a7aedf
aryanSmalhotra <84138957+aryanSmalhotra@users.noreply.github.com>
2021-09-20 21:35:20 +0530

    react app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+ReactDOM.render(
+  React.createElement("h1",
+   {style:{color:"green"}},
+  null, "Hello World"),
+  document.getElementById('root')
+);
+
