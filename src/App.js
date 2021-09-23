f211fda24ba6b52befb123ca75cce7800862f4aa
aryanSmalhotra <84138957+aryanSmalhotra@users.noreply.github.com>
2021-09-23 16:52:52 +0530

    valorant india ignition

--- src/App.js
+++ src/App.js
@@ -1,38 +1,52 @@
 
 import './App.css';
+import valorant from "./valorant india.webp"
 
-function Headers(){
+
+
+const teams =["Delhi Immortals",
+"Gujrat OPs",
+"Viva Mumbai",
+"Punjab Phantoms"];
+
+
+function Headers(props){
   return(
     <header>
-      <h1>Valorant Comes To India</h1>
+      <h1 style ={{textDecorationColor:"blue"}} >{props.name} Comes To India</h1>
       <h1>Valorant Ignition India 2021</h1>
+      <img src={valorant} height={250} alt ="Valorant is coming to India"/>
     </header>)
  
 }
-function Main(){
+function Main(prop){
   return(
    <section>
-     <p><h2>
-       Pick Up Your Phantoms and Vandals because On 21<sup>st</sup> It Starts</h2></p>
+     <p><h2  style ={{textDecorationColor:"blue"}}>
+       {prop.adjective} Up Your Phantoms and Vandals because On 21<sup>st</sup> It Starts</h2></p>
    </section>)
  
 }
-function Footer(){
+function Footer(props){
   return(
    <section>
-     <p>Teams Qualified</p>
+     <p>{props.qualified}</p>
+     <ul style={{textAlign: 'left'}}>
+       {props.teams.map((team, i) => 
+       (<li key ={i}>{team}</li>))}
+       </ul>
+     </section>)}
+ 
 
-   </section>)
- 
-}
+
 
 
 function App() {
   return (
     <div className="App">
-      <Headers />
-      < Main />
-      <Footer />
+      <Headers name ="Valorant" />
+      < Main  adjective = "Pick"/>
+      <Footer qualified = "Teams Qualified" teams={teams} />
 
     </div>
   );
