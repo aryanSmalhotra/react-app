9d8c7c12838d0e236fd1663631bcebbe4c735464
aryanSmalhotra <84138957+aryanSmalhotra@users.noreply.github.com>
2021-09-23 11:05:56 +0530

    valorant ignition

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+function Headers(){
+  return(
+    <header>
+      <h1>Valorant Comes To India</h1>
+      <h1>Valorant Ignition India 2021</h1>
+    </header>)
+ 
+}
+function Main(){
+  return(
+   <section>
+     <p><h2>
+       Pick Up Your Phantoms and Vandals because On 21<sup>st</sup> It Starts</h2></p>
+   </section>)
+ 
+}
+function Footer(){
+  return(
+   <section>
+     <p>Teams Qualified</p>
+
+   </section>)
+ 
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Headers />
+      < Main />
+      <Footer />
+
     </div>
   );
 }
