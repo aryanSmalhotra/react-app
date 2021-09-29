875fc7eb52285413bcfda7dbc39ec3380c403b91
aryanSmalhotra <84138957+aryanSmalhotra@users.noreply.github.com>
2021-09-29 16:26:42 +0530

    react

--- my-2ndapp/src/App.js
+++ my-2ndapp/src/App.js
@@ -1,20 +1,35 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function SecretFunction(){
-  return(
-  <h1>Secret Mouse In Pizza Recipe</h1>)
-};
 
-function RegularComponents(){
-  return(
-  <h1>Everyone Can See This</h1>
-  )
-};
 
-function App(props) {
+function App() {
+  const [emotion, setEmotion] = useState("No Game")
+  useEffect(() =>{
+    console.log(`The User is playing ${emotion} right now`);
+  });
   return(
     <>
-      {props.authorized ? <SecretFunction />:  <RegularComponents />} 
+      <h1>Aryan Singh Malhotra is currently playing {emotion}</h1>
+      <button style ={{display: 'flex', justifyContent: 'flex-center'}} onClick={() => setEmotion("Minecraft")}>
+        Minecraft
+        </button>
+        <hr></hr>
+      <button style ={{display: 'flex', justifyContent: 'flex-middle'}} onClick={() => setEmotion("CsGo")}>
+        CsGo
+        </button>
+        <hr></hr>
+      <button style ={{display: 'flex', justifyContent: 'flex-middle'}} onClick={() => setEmotion("Apex Legends")}>
+        Apex Legends
+        </button>
+        <hr></hr>
+      <button style ={{display: 'flex', justifyContent: 'flex-middle'}} onClick={() => setEmotion("Valorant")}>
+        Valorant
+        </button>
+        <hr></hr>
+      <button style ={{display: 'flex', justifyContent: 'flex-middle'}} onClick={() => setEmotion("No Game")}>
+        Default
+        </button>
   
 
   </>
